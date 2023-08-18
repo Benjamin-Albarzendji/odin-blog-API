ff854418177a978f7fd08a347179f14c7f02fd40
Benjamin-Albarzendji <105798796+Benjamin-Albarzendji@users.noreply.github.com>
2023-08-18 21:31:53 +0200

    Signup temporary created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -20,9 +21,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<h1>Create Post</h1>} />
           <Route path="/check-posts" element={<h1>Check Posts</h1>} />
+          <Route path="/signup" element={<SignUp />}></Route>
+
           <Route path="/login" element={<h1>Login</h1>} />
           <Route path="/logout" element={<h1>Logout</h1>} />
           <Route path="posts/:id" element={'hello'}></Route>
+          <Route path="/authors/:id" element={<h1>Author</h1>} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
